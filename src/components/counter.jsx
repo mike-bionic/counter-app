537a83b937c6a8e3881c43cb8ffe89e85967bf5d
mike <muhammedjepbarov@gmail.com>
2020-05-25 08:24:46 +0500

    list mapping and conditions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,32 +7,22 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
-    render() {
-        // let classes = this.getBadgeClasses();
-        return (
-            // we can directly write this.getBadgeClasses() 
-            // to use the method without defining again
-            <React.Fragment>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
-            </React.Fragment >
-        );
-    }
-    // this method was inside render() before we extracted it
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
+    renderTags() {
+        // handle the empty list case
+        if (this.state.tags.length === 0) return <p>No tags</p>; // or null
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     }
 
-    formatCount() {
-        // defining and dynamic style change
-        // if count = 0 -> warning else -> primary 
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+    render() {
+        return (
+            // adding condition statement and new message
+            // length is bool, 
+            // && is looking at the first condition and the second
+            <React.Fragment>
+                {this.state.tags.length === 0 && "Please create a new tag"}
+                {this.renderTags()}
+            </React.Fragment >
+        );
     }
 }
 
