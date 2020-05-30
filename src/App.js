c0c3710c61074cc59e092d00ea1ea7d48d378d61
mike <muhammedjepbarov@gmail.com>
2020-05-30 20:15:24 +0500

    state in the app and all components combined

--- src/App.js
+++ src/App.js
@@ -4,12 +4,55 @@ import Counters from './components/counters';
 import './App.css';
 
 class App extends Component {
+  state = {
+    counters: [
+      { id:1, value:0 },
+      { id:2, value:4 },
+      { id:3, value:0 },
+      { id:4, value:0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    // diffen objects "..." spread operator to clone the whole state
+    const counters = [...this.state.counters];
+    // find the index of current counter
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters })
+  };
+
+  // counterId is for selecting which counter to delete
+  // id should be passed on counter.jsx
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
+
   render() {
+    // we could say this.state.counters.length
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar 
+          totalCounters={this.state.counters.filter(c => c.value > 0).length} 
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
         </main>
       </React.Fragment>  
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,52 +2,18 @@ import React, { Component } from 'react';
 import Counter from "./counter"
 
 class Counters extends Component {
-	state = {
-		counters: [
-			{ id:1, value:0 },
-			{ id:2, value:4 },
-			{ id:3, value:0 },
-			{ id:4, value:0 }
-		]
-	};
-
-	handleIncrement = counter => {
-		// diffen objects "..." spread operator to clone the whole state
-		const counters = [...this.state.counters];
-		// find the index of current counter
-		const index = counters.indexOf(counter);
-		counters[index] = { ...counter };
-		counters[index].value++;
-		this.setState({ counters })
-	};
-
-	// counterId is for selecting which counter to delete
-	// id should be passed on counter.jsx
-	handleDelete = counterId => {
-		const counters = this.state.counters.filter(c => c.id !== counterId);
-		this.setState({ counters });
-	};
-
-
-	handleReset = () => {
-		const counters = this.state.counters.map(counter => {
-			counter.value = 0;
-			return counter;
-		});
-		this.setState({ counters });
-	};
-
 	render() {
 		return(
 			<div>
 				<button
-					onClick={this.handleReset}
+					onClick={this.props.onReset}
 					className="btn btn-primary btn-sm m-2">Reset</button>
-				{ this.state.counters.map(counter => (
+				{ this.props.counters.map(counter => (
 					<Counter 
-						key={counter.id} 
-						onDelete={this.handleDelete}
-						onIncrement={this.handleIncrement}
+						key={counter.id}
+						// bubbling the event to parent
+						onDelete={this.props.onDelete}
+						onIncrement={this.props.onIncrement}
 						// passing all properties of counter in one go
 						counter={counter}
 					/>
