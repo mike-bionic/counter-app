9fd18389075ca248850efa424723efdaadd36a88
mike <muhammedjepbarov@gmail.com>
2020-05-30 19:55:47 +0500

    Created navbar

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import Counters from './components/counters';
-
 ReactDOM.render(
-  <Counters />,
+  <App />,
   document.getElementById('root')
 );
